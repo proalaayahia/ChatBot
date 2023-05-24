92258cddf6634ec07a58f59282043964a3daba36
proalaayahia <ProAlaaYahia@outlook.com>
2023-05-24 16:18:14 +0300

    Add project files.

---
+++ DiscordChatBot/DiscordExtensions.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus;
+
+public static class DiscordExtensions
+{
+    private static AlaBot _Bot;
+
+    public static DiscordClient AddAlaBot(this DiscordClient client)
+    {
+        _Bot = new AlaBot();
+        _Bot.Initialize(client);
+
+        return client;
+    }
+}
---
+++ DiscordChatBot/Program.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+var config = new ConfigurationBuilder()
+    //.AddJsonFile("appsettings.json", false)
+    .AddUserSecrets(typeof(Program).Assembly, true)
+    .Build();
+
+var discord = new DiscordClient(new DiscordConfiguration
+{
+    AutoReconnect = true,
+    MinimumLogLevel = LogLevel.Debug,
+
+    Token = config["discordToken"],
+    TokenType = TokenType.Bot
+});
+discord.AddAlaBot();
+
+await discord.ConnectAsync();
+await Task.Delay(-1);
